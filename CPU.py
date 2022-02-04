98792e7ec650a707222918fae4b7cbe185e0be26
yejinK <8616197@gmail.com>
2022-02-04 17:07:31 -0500

    [Add] index addressing mode.

--- CPU.py
+++ CPU.py
@@ -2,116 +2,122 @@ from ast import Str
 from bdb import effective
 from math import ldexp
 from PC import ProgramCounter as PC
-from registers import MAR, MBR, IR, IndexRegister, GeneralRegister as GR
+from registers import MAR, MBR, MFR, IR, IndexRegister, GeneralRegister as GR
 from Memory import Memory
 
 
-#TODO: enforce memory constraints on all registers, PC, etc
-#implement behavior when using idx register and GR to load/store
+# TODO: enforce memory constraints on all registers, PC, etc
+# implement behavior when using idx register and GR to load/store
 
 class CPU:
 
-    def __init__(self,memsize=2048):
-        self.PC = PC(0) #starting addr from IPL.txt
+    def __init__(self, memsize=2048):
+        self.PC = PC(0)  # starting addr from IPL.txt
         self.GRs = [GR() for i in range(4)]
         self.IndexRegisters = [IndexRegister(0) for i in range(3)]
         self.MAR = MAR()
         self.MBR = MBR()
+        self.MFR = MFR()
         self.IR = IR()
+        self.memsize = memsize
         self.Memory = Memory(memsize)
-    
-    #TODO: modify to factor in index_register != 0
 
-    def LDR(self,operand, mode, general_register):
-        effective_addr = None
-        if mode == 0:
-            effective_addr = operand
-    
-        if mode == 1:
-            self.MAR.set_val(operand)
-            self.MBR.set_val(self.Memory.words[self.MAR.get_val()])
-            effective_addr = self.MBR.get_val()
+    # TODO: modify to factor in index_register != 0
+
+    def LDR(self, operand, index_register, mode, general_register):
+        effective_addr = self.get_effective_addr(operand, index_register, mode)
 
         self.MAR.set_val(effective_addr)
         self.MBR.set_val(self.Memory.words[self.MAR.get_val()])
         self.GRs[general_register].set_val(self.MBR.get_val())
-    
-    def STR(self, operand, mode, general_register):
-        effective_addr = operand
-        if mode == 0:
-            self.MAR.set_val(effective_addr)
-        if mode == 1:
-            self.MBR.set_val(self.Memory.words[self.MAR.get_val()])
-            effective_addr = self.MBR.get_val()
+
+    def STR(self, operand, index_register, mode, general_register):
+        effective_addr = self.get_effective_addr(operand, index_register, mode)
 
         self.MBR.set_val(self.GRs[general_register].get_val())
         self.Memory.words[effective_addr] = self.MBR.get_val()
-    
-    def LDA(self,operand, mode, general_register):
-        effective_addr = None
-        if mode == 0:
-            self.GRs[general_register].set_val(operand)
-        if mode == 1:
-            #addr = self.Memory.words[operand]
-            self.MAR.set_val(operand)
-            self.MBR.set_val(self.Memory.words[self.MAR.get_val()])
-            effective_addr = self.MBR.get_val()
-            self.GRs[general_register].set_val(effective_addr)
 
-    def LDX(self, operand, mode, index_register):
-        effective_addr = None
-        if mode == 0:
-            effective_addr = operand
-          
-        if mode == 1:
-            self.MAR.set_val(operand)
-            self.MBR.set_val(self.Memory.words[self.MAR.get_val()])
-            effective_addr = self.MBR.get_val()
-           
+    def LDA(self, operand, index_register, mode, general_register):
+        effective_addr = self.get_effective_addr(operand, index_register, mode)
+
+        self.GRs[general_register].set_val(effective_addr)
+
+    def LDX(self, operand, index_register, mode, general_register):
+        effective_addr = self.get_effective_addr(operand, index_register, mode)
+
         self.MBR.set_val(self.Memory.words[effective_addr])
         self.IndexRegisters[index_register].set_val(self.MBR.get_val())
 
-    def STX(self,operand, mode, index_register):
-        effective_addr = None
-        if mode == 0:
-            effective_addr = operand
-        if mode == 1:
-            self.MAR.set_val(operand)
-            self.MBR.set_val(self.Memory.words[self.MAR.get_val()])
-            effective_addr = self.MBR.get_val()
-        
+    def STX(self, operand, index_register, mode, general_register):
+        effective_addr = self.get_effective_addr(operand, index_register, mode)
+
         self.MAR.set_val(effective_addr)
         self.MBR.set_val(self.IndexRegisters[index_register].get_val())
         self.Memory[self.MAR.get_val()] = self.MBR.get_val()
-    
+
     def HALT(self):
         return -1
-    
-    #TODO: generator for each cycle 
+
+    def get_effective_addr(self, operand, index_register, mode):
+        if mode == 0:
+            if index_register == 0:
+                if self.check_addr(operand):
+                    return operand
+            else:
+                if self.check_addr(operand + self.IndexRegisters[index_register].get_val()):
+                    return operand + self.IndexRegisters[index_register].get_val()
+        elif mode == 1:
+            if index_register == 0:
+                if self.check_addr(operand):
+                    self.MAR.set_val(operand)
+                    self.MBR.set_val(self.Memory[self.MAR.get_val()])
+            else:
+                if self.check_addr(operand + self.IndexRegisters[index_register]):
+                    self.MAR.set_val(operand + self.IndexRegisters[index_register])
+                    self.MBR.set_val(self.Memory[self.MAR.get_val()])
+            return self.MBR.get_val()
+        return 0
+
+    def check_addr(self, addr):
+        # Reserved memory location.
+        if 0 <= addr < 6:
+            self.MFR.set_val(1)
+            return False
+        # Memory address is beyond the size of memory.
+        elif addr >= self.memsize:
+            self.MFR.set_val(3)
+            return False
+        return True
+
+    # TODO: generator for each cycle
     def step(self):
         self.MAR.set_val(self.PC.get_addr())
-        self.PC.increment_addr() #points to next instruction 
-        
+        self.PC.increment_addr()  # points to next instruction
+
         addr = self.MAR.get_val()
         self.MBR.set_val(self.Memory.words[addr])
-       
+
         self.IR.set_instruction(self.MBR.get_val())
-      
+
         opcode, operand, index_register, mode, general_register = self.IR.decode()
-        
+
         if opcode == 1:
-            self.LDR(operand, mode, general_register)
+            self.LDR(operand, index_register, mode, general_register)
         elif opcode == 2:
-            self.STR(operand, mode, general_register)
-        elif opcode == 3: 
-            self.LDA(operand, mode, general_register)
+            self.STR(operand, index_register, mode, general_register)
+        elif opcode == 3:
+            self.LDA(operand, index_register, mode, general_register)
         elif opcode == 33:
-            self.LDX(operand, mode, index_register)
+            self.LDX(operand, index_register, mode, general_register)
         elif opcode == 34:
-            self.STX(operand, mode, index_register)
+            self.STX(operand, index_register, mode, general_register)
         elif opcode == 0:
-            return -1
+            return self.HALT()
+        # Illegal opcode.
+        else:
+            self.MFR.set_val(2)
         return 0
+
 
 def main():
     cpu = CPU(2048)
@@ -119,6 +125,7 @@ def main():
     cpu.step()
     cpu.step()
     cpu.step()
+
 
 if __name__ == '__main__':
     main()
--- registers.py
+++ registers.py
@@ -1,19 +1,26 @@
-
 class Register:
-    
-    def __init__(self,val=0):
+
+    def __init__(self, val=0):
         self.val = val
 
-    def set_val(self,val):
+    def set_val(self, val):
         self.val = val
+
     def get_val(self):
         return self.val
+
 
 class MAR(Register):
     pass
 
+
 class MBR(Register):
     pass
+
+
+class MFR(Register):
+    pass
+
 
 class IR():
 
@@ -21,29 +28,34 @@ class IR():
         self.instruction = instruction
 
     def set_instruction(self, val):
-        self.instruction = val 
-    
+        self.instruction = val
+
     def get_instruction(self):
         return self.instruction
 
     def decode(self):
-        #instruction = hex_to_decimal(self.instruction)
-        #process self.instruciton
-        opcode = self.instruction % 2**6 
-        operand = self.instruction // 2**11
-        index_register = (self.instruction // 2*8) % 2**2
-        mode = (self.instruction // 2**10) % 2**1
-        general_register = (self.instruction // 2**6) % 2**2
+        # instruction = hex_to_decimal(self.instruction)
+        # process self.instruciton
+        opcode = self.instruction % 2 ** 6
+        operand = self.instruction // 2 ** 11
+        index_register = (self.instruction // 2 * 8) % 2 ** 2
+        mode = (self.instruction // 2 ** 10) % 2 ** 1
+        general_register = (self.instruction // 2 ** 6) % 2 ** 2
         return opcode, operand, index_register, mode, general_register
+
 
 class IndexRegister(Register):
     pass
 
+
 class GeneralRegister(Register):
     pass
+
 
 def main():
     m = MAR()
     print(m.get_val())
+
+
 if __name__ == '__main__':
     main()
