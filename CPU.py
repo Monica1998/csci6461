cac2efd56afbd992102f3b810d1f34c923c71622
Visheshj123 <visheshj123@gwmail.gwu.edu>
2022-02-03 23:12:00 -0500

    cleaned up

--- CPU.py
+++ CPU.py
@@ -2,11 +2,7 @@ from ast import Str
 from bdb import effective
 from math import ldexp
 from PC import ProgramCounter as PC
-from GR import GeneralRegister as GR
-from IndexRegister import IndexRegister
-from MAR import MAR 
-from MBR import MBR
-from IR import IR
+from registers import MAR, MBR, IR, IndexRegister, GeneralRegister as GR
 from Memory import Memory
 
 
@@ -27,73 +23,82 @@ class CPU:
     #TODO: modify to factor in index_register != 0
 
     def LDR(self,operand, mode, general_register):
-        val = None
+        effective_addr = None
         if mode == 0:
-            self.MAR.set_addr(operand)
-
+            effective_addr = operand
+    
         if mode == 1:
-            addr = self.Memory.words[operand]
-            self.MAR.set_addr(addr)
+            self.MAR.set_val(operand)
+            self.MBR.set_val(self.Memory.words[self.MAR.get_val()])
+            effective_addr = self.MBR.get_val()
 
+        self.MAR.set_val(effective_addr)
         self.MBR.set_val(self.Memory.words[self.MAR.get_val()])
         self.GRs[general_register].set_val(self.MBR.get_val())
     
     def STR(self, operand, mode, general_register):
+        effective_addr = operand
         if mode == 0:
-            self.MAR.set_addr(operand)
+            self.MAR.set_val(effective_addr)
         if mode == 1:
-            addr = self.Memory.words[operand]
-            self.MAR.set_addr(addr)
+            self.MBR.set_val(self.Memory.words[self.MAR.get_val()])
+            effective_addr = self.MBR.get_val()
 
         self.MBR.set_val(self.GRs[general_register].get_val())
-        self.Memory[self.MAR.get_addr()] = self.MBR.get_val()
+        self.Memory.words[effective_addr] = self.MBR.get_val()
     
     def LDA(self,operand, mode, general_register):
+        effective_addr = None
         if mode == 0:
-            self.MAR.set_val(operand)
-
+            self.GRs[general_register].set_val(operand)
         if mode == 1:
-            addr = self.Memory.words[operand]
-            self.MAR.set_addr(addr)
-
-        self.MBR.set_val(self.Memory.words[self.MAR.get_val()])
-        self.GRs[general_register].set_val(self.MBR.get_val())
+            #addr = self.Memory.words[operand]
+            self.MAR.set_val(operand)
+            self.MBR.set_val(self.Memory.words[self.MAR.get_val()])
+            effective_addr = self.MBR.get_val()
+            self.GRs[general_register].set_val(effective_addr)
 
     def LDX(self, operand, mode, index_register):
-        val = None
+        effective_addr = None
         if mode == 0:
-            self.MAR.set_addr(index_register)
+            effective_addr = operand
           
         if mode == 1:
-            addr = self.Memory.words[operand]
-            self.MAR.set_addr(addr)
+            self.MAR.set_val(operand)
+            self.MBR.set_val(self.Memory.words[self.MAR.get_val()])
+            effective_addr = self.MBR.get_val()
            
-        self.MBR.set_val(self.Memory.words[self.MAR.get_addr()])
-        self.IndexRegisters[index_register].set_val(val)
+        self.MBR.set_val(self.Memory.words[effective_addr])
+        self.IndexRegisters[index_register].set_val(self.MBR.get_val())
 
     def STX(self,operand, mode, index_register):
+        effective_addr = None
         if mode == 0:
-            self.MAR.set_addr(operand)
+            effective_addr = operand
         if mode == 1:
-            addr = self.Memory[operand]
-            self.MAR.set_addr(addr)
+            self.MAR.set_val(operand)
+            self.MBR.set_val(self.Memory.words[self.MAR.get_val()])
+            effective_addr = self.MBR.get_val()
+        
+        self.MAR.set_val(effective_addr)
         self.MBR.set_val(self.IndexRegisters[index_register].get_val())
-        self.Memory[self.MAR.get_addr()] = self.MBR.get_val()
+        self.Memory[self.MAR.get_val()] = self.MBR.get_val()
     
     def HALT(self):
         return -1
     
     #TODO: generator for each cycle 
     def step(self):
-        self.MAR.set_addr(self.PC.get_addr())
+        self.MAR.set_val(self.PC.get_addr())
         self.PC.increment_addr() #points to next instruction 
         
-        addr = self.MAR.get_addr()
+        addr = self.MAR.get_val()
         self.MBR.set_val(self.Memory.words[addr])
        
         self.IR.set_instruction(self.MBR.get_val())
       
         opcode, operand, index_register, mode, general_register = self.IR.decode()
+        
         if opcode == 1:
             self.LDR(operand, mode, general_register)
         elif opcode == 2:
@@ -111,6 +116,8 @@ class CPU:
 def main():
     cpu = CPU(2048)
     cpu.Memory.read_mem()
+    cpu.step()
+    cpu.step()
     cpu.step()
 
 if __name__ == '__main__':
---
+++ registers.py
@@ -0,0 +1,49 @@
+
+class Register:
+    
+    def __init__(self,val=0):
+        self.val = val
+
+    def set_val(self,val):
+        self.val = val
+    def get_val(self):
+        return self.val
+
+class MAR(Register):
+    pass
+
+class MBR(Register):
+    pass
+
+class IR():
+
+    def __init__(self, instruction=None):
+        self.instruction = instruction
+
+    def set_instruction(self, val):
+        self.instruction = val 
+    
+    def get_instruction(self):
+        return self.instruction
+
+    def decode(self):
+        #instruction = hex_to_decimal(self.instruction)
+        #process self.instruciton
+        opcode = self.instruction % 2**6 
+        operand = self.instruction // 2**11
+        index_register = (self.instruction // 2*8) % 2**2
+        mode = (self.instruction // 2**10) % 2**1
+        general_register = (self.instruction // 2**6) % 2**2
+        return opcode, operand, index_register, mode, general_register
+
+class IndexRegister(Register):
+    pass
+
+class GeneralRegister(Register):
+    pass
+
+def main():
+    m = MAR()
+    print(m.get_val())
+if __name__ == '__main__':
+    main()
